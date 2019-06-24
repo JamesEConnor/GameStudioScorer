460f04146f2f9878a604780c6f3c30b8b65fa949
James Connor <jamesegconnor@gmail.com>
2019-06-24 16:52:23 -0400

    Updated Program

--- GameStudioScorer/Secret Sauce/CrunchScorer.cs
+++ GameStudioScorer/Secret Sauce/CrunchScorer.cs
@@ -22,7 +22,8 @@ namespace GameStudioScorer.Crunch
 			LogarithmicEquation log = new LogarithmicEquation(1/exp.A, exp.r, employeeCount);
 			//Console.WriteLine(log.a + ", " + log.b + ", " + log.c);
 			//Console.WriteLine(MathUtils.LogarithmicIntegral(log, 1f, 8f) + "," + (7f * log.GetValue(8f)));
-			return MathUtils.LogarithmicIntegral(log, 1f, 8f)/(7f * log.GetValue(8f));
+			const int length = 7;
+			return MathUtils.LogarithmicIntegral(log, 1f, length + 1)/(length * log.GetValue(length + 1));
 		}
 	}
 }
@@ -35,9 +36,9 @@ namespace GameStudioScorer.Crunch
  */
 
 /* 
- * 0.927385129854	Rockstar		Worst
- * 0.89632079363	Schell			Best
- * 0.92178463022	Epic			2nd Worst
- * 0.919793170921	Bioware			Middle
- * 0.920726941583	CD Projekt Red	2nd Best
+ * 0.927385129854	Rockstar		Worst			0.9273852
+ * 0.89632079363	Schell			Best			0.8963208
+ * 0.92178463022	Epic			2nd Worst		0.9217846
+ * 0.919793170921	Bioware			Middle			0.9197932
+ * 0.920726941583	CD Projekt Red	2nd Best		0.920727
  */
